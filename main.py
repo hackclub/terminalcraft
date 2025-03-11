3b22542688a42b25af9550ee26e2b6f8f9a02082
HammerPot <shawivg@gmail.com>
2025-03-10 20:16:17 -0400

    user inputs for player count

--- main.py
+++ main.py
@@ -24,16 +24,48 @@ def setupGame(players, count, deck): # takes in a list of players and the count 
 
 rawDeck = json.load(open("deck.json")) # opens deck.json and assigns it
 
+# print ("UNSHUFFLED DECK:")
+# print(rawDeck)
+# print("\n\n\n")
 
-players = [ # list of players (make it dynamic from user input later!!)
-    {"name": "Player 1", "hand": []},
-    {"name": "Player 2", "hand": []},
-    {"name": "Player 3", "hand": []},
-    {"name": "Player 4", "hand": []}
-]
+# players = [ # list of players (make it dynamic from user input later!!)
+#     {"name": "Player 1", "hand": []},
+#     {"name": "Player 2", "hand": []},
+#     {"name": "Player 3", "hand": []},
+#     {"name": "Player 4", "hand": []}
+# ]
 
+def getPlayerCount():
+	players = []
+	try:
+		numPlayers = int(input("Enter the number of players: "))
+		for each in range(numPlayers):
+			playerName = input("Enter player " + str(each + 1) + "'s name: ")
+			players.append({"name": playerName, "hand": []})
+
+		return players
+	except ValueError:
+		print("Please enter a number")
+		getPlayerCount()
+
+players = getPlayerCount()
+
+print(players)
 newDeck = shuffleDeck(rawDeck) # shuffles the deck and assigns it
+# print ("SHUFFLED DECK:")
+# print(newDeck)
+# print("\n\n\n")
 
 hands, gameDeck = setupGame(players, 7, newDeck) # sets up the game and assigns the modified deck
+
+# print ("GAME DECK:")
+# print(gameDeck)
+# print("\n\n\n")
+
+# print ("PLAYERS:")
+# for player in players:
+# 	print(player["name"] + ": " + str(player["hand"]))
+
+
 
 
