71c88f4451563611d22f97c57c66250d4e742c33
Tanish Gambhir <tanishgambhir55@gmail.com>
2025-03-20 01:36:04 +0530

    tic-tac-toe.c

--- submissions/tic-tac-toe/tic-tac-toe.c
+++ submissions/tic-tac-toe/tic-tac-toe.c
@@ -4,41 +4,43 @@
 #include <ctype.h>
 
 char board[3][3];
-char PLAYER;
-char COMPUTER;
+char PLAYER, PLAYER2, COMPUTER;
+int is_multiplayer = 0;
 
 void resetBoard();
 void printBoard();
 int getFreeSpaces();
-void playerMove();
+void playerMove(int player);
 void computerMove();
 char checkWinner();
 void printWinner(char);
 void character();
 void menu();
+void selectMode();
 
 int main(){
-    menu();
-    character();
-    char winner = ' ';
-    char response = ' ';
-
-    do
-    {
-        winner = ' ';
-        response = ' ';
+    char response;
+    do {
+        menu();
+        selectMode();
+        character();
+        char winner = ' ';
         resetBoard();
     
         while(winner == ' ' && getFreeSpaces() != 0){
             printBoard();
-            playerMove();
+            playerMove(1);
     
             winner = checkWinner();
             if(winner != ' ' || getFreeSpaces() == 0){
                 break;
             }
     
-            computerMove();
+            if (is_multiplayer) {
+                playerMove(2);
+            } else {
+                computerMove();
+            }
     
             winner = checkWinner();
             if(winner != ' ' || getFreeSpaces() == 0){
@@ -51,11 +53,9 @@ int main(){
         printf("\nWould you like to play again? (Y/N): ");
         scanf(" %c", &response);
         response = toupper(response);
-        character();
     } while (response == 'Y');
 
-    printf("Thanks for playing!");
-
+    printf("Thanks for playing!\n");
     return 0;
 }
 
@@ -66,22 +66,18 @@ void resetBoard(){
         }
     }
 }
+
 void printBoard(){
     printf("\n");
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c  \n", board[0][0], board[0][1], board[0][2]);
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c  \n", board[1][0], board[1][1], board[1][2]);
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c  \n", board[2][0], board[2][1], board[2][2]);
-    printf("     |     |     \n");
+    for(int i = 0; i < 3; i++){
+        printf("  %c  |  %c  |  %c  \n", board[i][0], board[i][1], board[i][2]);
+        if(i < 2) printf("-----|-----|-----\n");
+    }
     printf("\n");
 }
+
 int getFreeSpaces(){
     int freespaces = 9;
-
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             if(board[i][j] != ' '){
@@ -91,126 +87,99 @@ int getFreeSpaces(){
     }
     return freespaces;
 }
-void playerMove(){
-    int x;
-    int y;
 
-    do
-    {
-        printf("Enter the row number #(1-3): ");
+void playerMove(int player){
+    int x, y;
+    char symbol = (player == 1) ? PLAYER : PLAYER2;
+
+    do {
+        printf("Player %d (%c), enter row #(1-3): ", player, symbol);
         scanf("%d", &x);
         x--;
-        printf("Enter the column number #(1-3): ");
+        printf("Enter column #(1-3): ");
         scanf("%d", &y);
         y--;
     
         if(board[x][y] != ' '){
-            printf("Invalid move\n");
-        }
-        else{
-            board[x][y] = PLAYER;
+            printf("Invalid move, try again.\n");
+        } else {
+            board[x][y] = symbol;
             break;
         }
-    } while (board[x][y] != ' ');
-    
+    } while (1);
 }
+
 void computerMove(){
     srand(time(0));
-    int x;
-    int y;
+    int x, y;
+    printf("Computer is thinking...\n");
     
     if(getFreeSpaces() > 0){
-        do
-        {
+        do {
             x = rand() % 3;
             y = rand() % 3;
         } while (board[x][y] != ' ');
         board[x][y] = COMPUTER;
     }
-    else{
-        printBoard(' ');
-    }
 }
+
 char checkWinner(){
-    // rows
     for(int i = 0; i < 3; i++){
-        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != ' ')
             return board[i][0];
-        }
-    }
-
-    // columns
-    for(int i = 0; i < 3; i++){
-        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != ' ')
             return board[0][i];
-        }
     }
-
-    // diagnols
-    for(int i = 0; i < 3; i++){
-        if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
-            return board[0][0];
-        }
-    }
-    for(int i = 0; i < 3; i++){
-        if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
-            return board[0][2];
-        }
-    }
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != ' ')
+        return board[0][0];
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != ' ')
+        return board[0][2];
     return ' ';
 }
+
 void printWinner(char winner){
     if(winner == PLAYER){
-        printf("YOU WIN!");
+        printf("PLAYER 1 WINS!\n");
+    }
+    else if(winner == PLAYER2){
+        printf("PLAYER 2 WINS!\n");
     }
     else if(winner == COMPUTER){
-        printf("COMPUTER WINS!");
+        printf("COMPUTER WINS!\n");
     }
     else{
-        printf("ITS A TIE!");
+        printf("IT'S A TIE!\n");
     }
 }
+
 void menu(){
-    char response_user = ' ';
-    printf("\n\n----------------------- WELCOME TO TIC-TAC-TOE ----------------------------------");
-    printf("\n--------------------------- Made by Tanish ---------------------------------------");
-
-    printf("\nDo you want to play the game?: (Y/N): ");
-    scanf(" %c", &response_user);
-    response_user = toupper(response_user);
-
-    if(response_user == 'Y'){
-        return;
-    }
-    else if(response_user == 'N'){
+    char response;
+    printf("\nWELCOME TO TIC-TAC-TOE\n");
+    printf("Do you want to play? (Y/N): ");
+    scanf(" %c", &response);
+    response = toupper(response);
+    if(response != 'Y'){
         printf("You have exited the game!\n");
         exit(0);
     }
-    else{
-        printf("Invalid Input!");
-        menu();
-    }
 }
+
+void selectMode(){
+    int choice;
+    printf("Select Mode: \n1. Single Player (vs AI)\n2. Multiplayer (Two Players)\nEnter choice: ");
+    scanf("%d", &choice);
+    is_multiplayer = (choice == 2);
+}
+
 void character(){
-    char response = ' ';
-    printf("\nWhat do you want to choose? (X/O): ");
+    char response;
+    printf("\nChoose your symbol (X/O): ");
     scanf(" %c", &response);
     response = toupper(response);
-
-    if(response == 'X'){
-        PLAYER = 'X';
-        COMPUTER = 'O';
-        return;
-    }
-    else if(response == 'O'){
-        PLAYER = 'O';
-        COMPUTER = 'X';
-        return;
-    }
-    else{
-        printf("Invalid option!");
-        character();
+    PLAYER = (response == 'X') ? 'X' : 'O';
+    if(is_multiplayer) {
+        PLAYER2 = (PLAYER == 'X') ? 'O' : 'X';
+    } else {
+        COMPUTER = (PLAYER == 'X') ? 'O' : 'X';
     }
 }
-
-// Code completed
