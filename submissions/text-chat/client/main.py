67d799baf3de6c2a5f4dad2ef0760a195e95438d
Abdo1713 <142174677+Abdo1713@users.noreply.github.com>
2025-03-24 22:38:48 +0200

    Update main.py

--- submissions/text-chat/client/main.py
+++ submissions/text-chat/client/main.py
@@ -2,14 +2,13 @@ import socket
 import threading
 import os
 import shutil
+import struct
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = ''
 ip_address = input("please enter the server's private IP address")
-client.connect((ip_address, 2000))#ip address here
-project = os.getcwd()
+client.connect((ip_address, 5000))#ip address here
 filename = ""
 filepath = ""
-new = ""
 def receive():
     while True:
         try:
@@ -18,29 +17,27 @@ def receive():
                 print(data.decode('utf-8'))
         except:
             continue
-def send_file(filepath, new):
-    new_filepath = os.path.join(project, new)
-    shutil.copy(filepath, new_filepath)
+
+
+def send_file(filepath, client):
+    file_name = os.path.basename(filepath).encode('utf-8')
+    file_size = os.path.getsize(filepath)
     client.send("sending...".encode('utf-8'))
-    with open(new, "rb") as file:
-        file_size = os.path.getsize(new)
-        client.send(new.encode())
-        client.send(str(file_size).encode())
-        file_data = file.read()
-        client.sendall(file_data)
-    client.send(b"<LOL>")
-    print(f"File {new} sent successfully.")
+    client.send(struct.pack("I", len(file_name)))
+    client.send(file_name)
+    client.send(struct.pack("Q", file_size))
+    with open(filepath, "rb") as file:
+        while chunk := file.read(4096):
+            client.send(chunk)
 
-
+    print(f"File '{filepath}' sent successfully.")
 while True:
     thread_receive = threading.Thread(target=receive, daemon=True).start()
     data = input()
     if data == "send_file":
         print("ENTER FILE PATH:")
         filepath = input()
-        print("Enter new name for image (must end with the extension type):")
-        new = input()
-        send_file(filepath, new)
+        send_file(filepath, client)
     elif data == "exit_chat":
         print("Closing connection...")
         client.close()
