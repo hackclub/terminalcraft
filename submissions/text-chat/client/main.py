c4f62588fddac774aa2325c48846fa283322fd2f
Abdo1713 <142174677+Abdo1713@users.noreply.github.com>
2025-03-28 01:19:38 +0200

    Update main.py

--- submissions/text-chat/client/main.py
+++ submissions/text-chat/client/main.py
@@ -6,7 +6,7 @@ import struct
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = ''
 ip_address = input("please enter the server's private IP address")
-client.connect((ip_address, 9000))#ip address here
+client.connect((ip_address, 1000))#ip address here
 filename = ""
 filepath = ""
 def receive():
@@ -20,15 +20,11 @@ def receive():
 
 
 def send_file(filepath, client):
-    file_name = os.path.basename(filepath).encode('utf-8')
+    file_name = os.path.basename(filepath)
     file_size = os.path.getsize(filepath)
     client.send("sending...".encode('utf-8'))
-    ack = client.recv(1024)  # Add this line
-    if ack.decode() != "READY":
-        print("Server not ready, aborting file transfer.")
-        return
     client.send(struct.pack("!I", len(file_name)))
-    client.send(file_name)
+    client.send(file_name.encode())
     client.send(struct.pack("!Q", file_size))
     with open(filepath, "rb") as file:
         while chunk := file.read(4096):
