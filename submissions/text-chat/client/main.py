8033766c720a3bb52956b4279d71c6895760be8c
Abdo1713 <142174677+Abdo1713@users.noreply.github.com>
2025-03-28 17:49:15 +0200

    Update main.py

--- submissions/text-chat/client/main.py
+++ submissions/text-chat/client/main.py
@@ -5,7 +5,7 @@ import struct
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = ''
 ip_address = input("please enter the server's private IP address")
-client.connect((ip_address, 1000))#ip address here
+client.connect((ip_address, 9000))#ip address here
 filename = ""
 filepath = ""
 def receive():
